a761cb3cac0e337619b8ab43a1edecdd89e106ec
yanghaotian <yanghaotian07@gmail.com>
2017-09-05 10:42:58 +0800

    upd: constant semicolon

--- constant/index.js
+++ constant/index.js
@@ -1,2 +1,2 @@
-export const BASE_URL    = 'https://www.nhtsa.gov/webapi/api/SafetyRatings/'
-export const BASE_PARAMS = 'format=json'
+export const BASE_URL    = 'https://www.nhtsa.gov/webapi/api/SafetyRatings/';
+export const BASE_PARAMS = 'format=json';
