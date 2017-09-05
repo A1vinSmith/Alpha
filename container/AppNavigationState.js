6f19e9b0992a47efd75e709f4dd401b3acefc4c2
yanghaotian <yanghaotian07@gmail.com>
2017-09-05 21:34:04 +0800

    add: expo Audio

--- container/AppNavigationState.js
+++ container/AppNavigationState.js
@@ -1,14 +1,26 @@
-import React                    from "react"
+import React, { Component }     from "react"
 import { connect }              from "react-redux"
 import { addNavigationHelpers } from "react-navigation"
+import { Audio }                from 'expo';
 
 import AppNavigator             from "../component/AppNavigator"
 
-/*
+
 @connect(state => ({
     nav: state.nav
 }))
 class AppNavigationState extends Component {
+    async componentWillMount() {
+        const source = require('../component/login/assets/mp3/moonLight.mp3');
+        try {
+            await Audio.setIsEnabledAsync(true);
+            const sound = new Audio.Sound();
+            await sound.loadAsync(source);
+            await sound.playAsync();
+        } catch(error) {
+            console.error(error);
+        }
+    }
     render() {
         return (
             <AppNavigator
@@ -22,7 +34,8 @@ class AppNavigationState extends Component {
 }
 
 export default AppNavigationState
-*/
+
+/*
 const AppNavigationState = ({ dispatch, nav }) => (
     <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
 );
@@ -30,5 +43,6 @@ const AppNavigationState = ({ dispatch, nav }) => (
 const mapStateToProps = state => ({
     nav: state.nav,
 });
-  
+
 export default connect(mapStateToProps)(AppNavigationState)
+*/
