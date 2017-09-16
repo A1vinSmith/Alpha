716e972f0072e70b67bebf5f5bd9f72cd7b1137e
yanghaotian <yanghaotian07@gmail.com>
2017-09-16 17:51:39 +0800

    add: TextInput to Login for userName

--- component/login/LoginScreen.js
+++ component/login/LoginScreen.js
@@ -1,48 +1,65 @@
-import React      from 'react'
-import PropTypes  from 'prop-types'
+import React, { Component } from 'react'
+import PropTypes            from 'prop-types'
 import { 
-  Button, 
-  StyleSheet, 
-  Text, 
-  View }          from 'react-native'
+    Button, StyleSheet,
+    Text, TextInput, View } from 'react-native'
 
-import * as types from '../../constant/ActionType'
+import * as types           from '../../constant/ActionType'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
 
-const LoginScreen = ({ navigation }) => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>
-      Screen A
-    </Text>
-    <Text style={styles.instructions}>
-      This is great
-    </Text>
-    <Button
-      onPress={() => navigation.dispatch({ type: types.LOGIN })}
-      title="Log in"
-    />
-  </View>
-);
+class LoginScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: ''};
+    }
+
+    render() {
+        const { navigation } = this.props;
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Screen A
+                </Text>
+                <TextInput
+                    style={{height: 40}}
+                    placeholder="Type here to translate!"
+                    onChangeText={(text) => this.setState({text})}
+                />
+                <Text style={{padding: 10, fontSize: 42}}>
+                    {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+                </Text>
+                <Text style={styles.instructions}>
+                    This is great
+                </Text>
+                <Button
+                    onPress={() => navigation.dispatch({ type: types.LOGIN })}
+                    title="Log in"
+                />
+            </View>
+        )
+    }
+}
 
 LoginScreen.propTypes = {
   navigation: PropTypes.object.isRequired,
 };
 
 LoginScreen.navigationOptions = {
-  title: 'Log In',
+  header: null,
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
 
 export default LoginScreen
--- design/product.md
+++ design/product.md
@@ -1,2 +1,3 @@
 ## Rule
 1. Auto save
+2. Choose difficult ??? NO NO NO
