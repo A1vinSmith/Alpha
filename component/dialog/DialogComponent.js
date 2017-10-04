8ba41a516c80d81c36c600d1b7637a412ca1c2c3
yanghaotian <yanghaotian07@gmail.com>
2017-10-04 23:43:14 +0800

    upd: with PropTypes.shape

--- component/dialog/DialogComponent.js
+++ component/dialog/DialogComponent.js
@@ -6,7 +6,11 @@ import {
 
 export default class DialogComponent extends Component {
     static propTypes = {
-        dialog: PropTypes.object.isRequired
+        dialog: PropTypes.shape({
+            character: PropTypes.string.isRequired,
+            lines: PropTypes.array.isRequired,
+            sentence: PropTypes.number.isRequired
+        }).isRequired
     };
 
     render() {
@@ -34,14 +38,15 @@ const styles = StyleSheet.create({
         borderRadius: 10, borderWidth: 5, borderColor: 'rgba(0,0,0,0.5)',
         fontSize: 14, alignSelf: 'flex-end',
         marginBottom: 10,
-        backgroundColor: 'rgba(28,38,43,1.0)',
-        maxWidth: 280,
+
+        backgroundColor: 'rgba(28,38,43,1.0)',  // Make special border with this
+        maxWidth: 280,                          // Set max value
     },
     character: {
         color: 'rgba(157,157,0,0.9)', textAlign: 'center', textAlignVertical:'center',
+        paddingTop: 3, paddingBottom: 1, paddingRight: 3, paddingLeft: 3,
         borderRadius: 11, borderWidth: 2.5, borderColor: 'rgba(225,172,72,0.6)',
         fontSize: 16, alignSelf: 'flex-end',
         marginBottom: 10,
-        paddingTop: 3, paddingBottom: 1, paddingRight: 3, paddingLeft: 3,
     }
 });
